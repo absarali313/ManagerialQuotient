6f6d92d7b65ac722841ed35bbd55b774205d0df6
shayaanshahid <cadetshayaan@gmail.com>
2026-04-29 18:41:01 +0500

    Update assessments index to explicitly pass variables to partials

--- resources/views/livewire/assessments/index.blade.php
+++ resources/views/livewire/assessments/index.blade.php
@@ -1,5 +1,11 @@
 <div class="space-y-8">
+
     @include('livewire.assessments.partials.header')
+
     @include('livewire.assessments.partials.stats')
-    @include('livewire.assessments.partials.table')
+
+    @include('livewire.assessments.partials.table', [
+        'assessments' => $assessments
+    ])
+
 </div>
